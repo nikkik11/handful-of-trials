71dbcaffad192c8b3d7d7bf1fe28fb77cdff4a6b
Nikki Kuang <nikki.kuang@gmail.com>
2019-07-19 16:18:24 -0700

    Revert "Revert "Script notebooks""
    This reverts commit d85003b7577d10dfb65ce08427277acd511ef8d9.

---
+++ Mujoco_py Install Related/Modified Files/Mujoco_py/platname_targdir.py
@@ -0,0 +1,10 @@
+import sys
+if sys.platform.startswith("darwin"):
+    platname = "osx"
+elif sys.platform.startswith("linux"):
+    platname = "linux"
+elif sys.platform.startswith("windows"):
+    platname = "win"
+platname = "win"
+targdir = "mujoco_%s"%platname
+
